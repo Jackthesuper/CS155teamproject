ea5d9577f52e402ba82a9a0accc1355bcbffd924
Jackthesuper <jack.shenzijie@outlook.com>
2018-04-18 22:26:34 -0400

    collisionOncemore

--- PA03/initAnimation.js
+++ PA03/initAnimation.js
@@ -50,8 +50,8 @@ function createMainScene(){
     wall.position.set(10,0,10);
     //scene.add(wall);
 
-    initCoinOBJ(20,0,-120);
-    initCoinOBJ(-20,0,-120);
+    initCoinOBJ(20,5,-120);
+    initCoinOBJ(-20,5,-120);
 }
 
 function createEndScene(){

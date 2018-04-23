7b1e3305307ea584e2c96998caa6c267eb208b87
helloCynthia <xywang19@brandeis.edu>
2018-04-23 02:46:21 -0400

    addMoreCoins

--- PA03/coinsInRightBigRoom.js
+++ PA03/coinsInRightBigRoom.js
@@ -1,4 +1,6 @@
 function coinsInRightBigRoom(){
+
+      //big circle
       initCoinOBJ(20,5,-220);
       initCoinOBJ(220,5,-220);
       //initCoinOBJ(20,5,-200-10);
@@ -10,6 +12,15 @@ function coinsInRightBigRoom(){
       for(j=1;j<20;j++){
         initCoinOBJ(20+10*j,5,-220);
         initCoinOBJ(20+10*j,5,-420);
+      }
+
+      //small circle
+      initCoinOBJ(60,5,-260);
+      initCoinOBJ(150,5,-260);
+
+      for(k=1;k<=10;i++){
+        initCoinOBJ(60,5,-260-10*k);
+        initCoinOBJ(150,5,-260-10*k);
       }
 
 

5fc2d68227cc993c2115dc7b5879f68eece00fb0
frankkdong <frankdong@brandeis.edu>
2018-04-26 00:58:43 -0400

    destination

--- PA03/initAnimation.js
+++ PA03/initAnimation.js
@@ -125,6 +125,7 @@ function createMainScene(){
     avatarCam = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 1000 );
     //avatar = createAvatar();
     createAvatar();
+    destination();
     addAllNPC();
     addAllfans();
     addAllTowers();

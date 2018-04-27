0d854fdc4cbc0db46daa4121952ee55add031a3b
Jackthesuper <jack.shenzijie@outlook.com>
2018-04-27 15:23:06 -0400

    pinkBoxAlert

--- FinalProject/createTricker.js
+++ FinalProject/createTricker.js
@@ -2,10 +2,11 @@ function createTricker(){
   var geometry = new THREE.BoxGeometry(10,10,10);
   var material= new THREE.MeshLambertMaterial( { color: 0xff00ff});
   var mesh = new Physijs.BoxMesh( geometry, material,0);
+  var touch =0;
   mesh.position.set(270,5,-200);
   mesh.addEventListener('collision',
       function (other_object){
-        if(other_object ==avatar && gameState.score>5 &&tricker==false){
+        if(other_object ==avatar && gameState.score>4 &&tricker==false){
                   for(var o=0;o<3;o++){
                     soundEffect('good1.mp3');
                     fans[o].__dirtyPosition = true;
@@ -15,6 +16,10 @@ function createTricker(){
                   mesh.__dirtyPosition=true;
                   mesh.translateY(-100);
                   tricker = true;
+        }
+        else if(other_object==avatar&&touch==0){
+          window.alert("Try to collect "+(5-gameState.score)+ " more coins and then come back to touch the pink box!")
+          touch =touch+1;
         }
       })
   scene.add(mesh);
